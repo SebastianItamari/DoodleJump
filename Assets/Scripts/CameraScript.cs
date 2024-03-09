e1911b21bbd679ab300910f602ee60a3771bfd5b
SebastianItamari <sebaton66@gmail.com>
2024-03-09 18:50:09 -0400

    Fifth Version.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -12,12 +12,15 @@ public class CameraScript : MonoBehaviour
     private Transform targetTransform;
     private GameObject bg;
     private Transform bgTransform;
+    private int index = 0;
+    private bool change = false;
 
     void Start()
     {
-        bg = (GameObject)Instantiate(backgrounds[0].background, new Vector2(0,0), Quaternion.identity);
-        gameController.varY = backgrounds[0].varY;
-        gameController.speedPlatforms = backgrounds[0].horizontalSpeeds;
+        bg = (GameObject)Instantiate(backgrounds[index].background, new Vector2(0,0), Quaternion.identity);
+        gameController.varY = backgrounds[index].varY;
+        gameController.speedPlatforms = backgrounds[index].horizontalSpeeds;
+        index++;
         targetObject = GameObject.FindWithTag("Player");
 
         bgTransform = bg.GetComponent<Transform>();
@@ -40,12 +43,30 @@ public class CameraScript : MonoBehaviour
 
     private void LateUpdate()
     {
-        if(targetTransform.position.y > transform.position.y)
+        if (Mathf.RoundToInt(targetTransform.position.y) > 50 * index)
+        {
+            CreateNewBackground();
+        }
+
+        if (targetTransform.position.y > transform.position.y)
         {
             Vector3 newPosition = new Vector3(transform.position.x, targetTransform.position.y,transform.position.z);
             transform.position = newPosition;
             Vector3 newBackgroundPosition = new Vector3(bgTransform.position.x, targetTransform.position.y, bgTransform.position.z);
             bgTransform.position = newBackgroundPosition;
+        }
+    }
+
+    private void CreateNewBackground()
+    {
+        if (index < backgrounds.Count)
+        {
+            bg = Instantiate(backgrounds[index].background, new Vector2(0, targetTransform.position.y + 10), Quaternion.identity);
+            gameController.varY = backgrounds[index].varY;
+            gameController.speedPlatforms = backgrounds[index].horizontalSpeeds;
+
+            bgTransform = bg.GetComponent<Transform>();
+            index++;
         }
     }
 }
